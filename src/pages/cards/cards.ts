8e0a34439494b27ff008a07d360e90394f7061f5
surendra <persaudsurendra@gmail.com>
2017-03-06 20:58:03 -0500

    Got the cards working

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Cards page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   templateUrl: 'cards.html'
 })
@@ -15,10 +10,6 @@ export class CardsPage {
 
   constructor(public navCtrl: NavController, public params: NavParams) {
     this.card = params.get('card');
-  }
-
-  ionViewDidLoad() {
-    console.log(this.card);
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -34,35 +34,37 @@ export class HomePage {
     {
       cardTitle: "All Star Code Summer Intensive opens",
       cardContent: "All Star Code empowers young men with the skills, networks, and mindsets they need.",
-      cardThumbnail: "",
+      cardImage: "http://www.allstarcode.org/wp-content/uploads/2014/10/july07.jpg",
       cardProgramType: "SUMMER PROGRAM",
+      cardArticle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper, lacus ut tempus semper, dolor lectus dictum mi, sed pretium dolor sem non nisi. Nulla semper, quam et efficitur lacinia, mi urna rhoncus leo, et pharetra risus velit ac quam. Duis vel nisi vitae purus suscipit egestas vitae sit amet metus. Fusce ac quam at sem tempor commodo. Curabitur vel nisl ac nulla faucibus laoreet. Proin hendrerit finibus efficitur. Sed vehicula, lectus sit amet iaculis feugiat, ipsum nisl semper eros, nec dictum sapien felis sagittis augue. Donec a vestibulum leo. Phasellus in sapien eu neque dapibus scelerisque eu tincidunt ipsum. Duis fringilla felis elit, in pretium magna elementum sed. Nulla ac erat vestibulum, commodo felis quis, dignissim ex. Maecenas in nisl blandit, imperdiet diam vitae, sagittis arcu.",
     },
     {
       cardTitle: "DDC at Columbia University program opens",
       cardContent: "The Double Discovery Center at Columbia College works with low-income and first generation college bound Manhattan area youth from ages 12-27 to ensure high school graduation, college enrollment and completion and responsible adulthood.",
-      cardThumbnail: "",
+      cardImage: "https://ddc.college.columbia.edu/sites/default/files/styles/front_photo/public/2016%20DDC%20Graduation0015.JPG?itok=PxHaMvy2",
       cardProgramType: "COMMUNITY PROGRAM",
-
+      cardArticle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper, lacus ut tempus semper, dolor lectus dictum mi, sed pretium dolor sem non nisi. Nulla semper, quam et efficitur lacinia, mi urna rhoncus leo, et pharetra risus velit ac quam. Duis vel nisi vitae purus suscipit egestas vitae sit amet metus. Fusce ac quam at sem tempor commodo. Curabitur vel nisl ac nulla faucibus laoreet. Proin hendrerit finibus efficitur. Sed vehicula, lectus sit amet iaculis feugiat, ipsum nisl semper eros, nec dictum sapien felis sagittis augue. Donec a vestibulum leo. Phasellus in sapien eu neque dapibus scelerisque eu tincidunt ipsum. Duis fringilla felis elit, in pretium magna elementum sed. Nulla ac erat vestibulum, commodo felis quis, dignissim ex. Maecenas in nisl blandit, imperdiet diam vitae, sagittis arcu.",
     },
     {
       cardTitle: "LEDA summer program at Princeton University opens",
       cardContent: "LEDA is a national, not-for-profit organization dedicated to identifying and developing the academic and leadership potential of exceptional public high school students.",
-      cardThumbnail: "",
+      cardImage: "https://www.princeton.edu/main/images/news/2016/12/IMG_7143_1150.jpg",
       cardProgramType: "SUMMER PROGRAM",
-
-      
+      cardArticle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper, lacus ut tempus semper, dolor lectus dictum mi, sed pretium dolor sem non nisi. Nulla semper, quam et efficitur lacinia, mi urna rhoncus leo, et pharetra risus velit ac quam. Duis vel nisi vitae purus suscipit egestas vitae sit amet metus. Fusce ac quam at sem tempor commodo. Curabitur vel nisl ac nulla faucibus laoreet. Proin hendrerit finibus efficitur. Sed vehicula, lectus sit amet iaculis feugiat, ipsum nisl semper eros, nec dictum sapien felis sagittis augue. Donec a vestibulum leo. Phasellus in sapien eu neque dapibus scelerisque eu tincidunt ipsum. Duis fringilla felis elit, in pretium magna elementum sed. Nulla ac erat vestibulum, commodo felis quis, dignissim ex. Maecenas in nisl blandit, imperdiet diam vitae, sagittis arcu.",
     },
     {
       cardTitle: "The Posse Foundation application opens",
       cardContent: "An 8-month pre-collegiate training program during the academic school year",
-      cardThumbnail: "",
+      cardImage: "http://nationswell.com/wp-content/uploads/2014/01/posse-foundation-1.jpg",
       cardProgramType: "COMMUNITY PROGRAM",
-
+      cardArticle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper, lacus ut tempus semper, dolor lectus dictum mi, sed pretium dolor sem non nisi. Nulla semper, quam et efficitur lacinia, mi urna rhoncus leo, et pharetra risus velit ac quam. Duis vel nisi vitae purus suscipit egestas vitae sit amet metus. Fusce ac quam at sem tempor commodo. Curabitur vel nisl ac nulla faucibus laoreet. Proin hendrerit finibus efficitur. Sed vehicula, lectus sit amet iaculis feugiat, ipsum nisl semper eros, nec dictum sapien felis sagittis augue. Donec a vestibulum leo. Phasellus in sapien eu neque dapibus scelerisque eu tincidunt ipsum. Duis fringilla felis elit, in pretium magna elementum sed. Nulla ac erat vestibulum, commodo felis quis, dignissim ex. Maecenas in nisl blandit, imperdiet diam vitae, sagittis arcu.",
     },
 
   ]
     viewCard(card){
-    this.navCtrl.push(CardsPage)
+    this.navCtrl.push(CardsPage,{
+      card: card
+    });
   }
 
 }
